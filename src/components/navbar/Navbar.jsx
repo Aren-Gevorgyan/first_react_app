c5a121d9aa065f6fa3bbd6fe41cff31262ba219f
Aren <63490565+Aren-Gevorgyan@users.noreply.github.com>
2021-02-26 18:55:20 +0400

    set photo friends

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,9 +3,10 @@ import style from './Navbar.module.css';
 import NavbarItem from './NavbarItem/NavbarItem';
 
 const Navbar = (props)=>{
-  
-   let itemMenu = props.menuItem.map(value => (<NavbarItem name={value.name} path={value.path}/>))
 
+   let itemMenu = props.menuItem.map(value => (
+   <NavbarItem name={value.name} path={value.path} friends={value.friends}/>))
+  
    return( 
    <nav>
 
--- src/components/navbar/NavbarItem/NavbarItem.jsx
+++ src/components/navbar/NavbarItem/NavbarItem.jsx
@@ -3,9 +3,22 @@ import style from './NavbarItem.module.css';
 import { NavLink } from 'react-router-dom';
 
 const NavbarItem = (props) => {
+    let friendsItem;
+
+    if(props.friends !== undefined){
+
+        friendsItem = props.friends.map(value => (
+                 <img className={style} src={value} alt="friend photo"/>
+            )) 
+
+    }else{
+        friendsItem = "";
+    }
+
     return(
-        <li className={style.item, style.active}>
+        <li className={style.item, style.active, style.container}>
             <NavLink to={`/${props.path}`} activeClassName={style.active}>{props.name}</NavLink>
+            <div>{friendsItem}</div>
         </li>
     )
 }
